5025135bb7858464695aa3ab5e70086723388224
Noah Lloyd <noahlloydrobson@gmail.com>
2022-04-15 21:26:58 +0200

    editable images

--- image/main.js
+++ image/main.js
@@ -1,6 +1,13 @@
 const imageLink = document.getElementById("img");
 const link = document.getElementById("link");
 const btn = document.getElementById("createWidget");
+setTimeout(() => {
+    const edit = document.getElementById('edit')
+    edit.addEventListener('click', ()=> {
+        localStorage.setItem('image', '')
+        location.reload()
+    })
+}, 200)
 
 const alreadyMade = localStorage.getItem("image");
 if (alreadyMade) {
@@ -8,7 +15,14 @@ if (alreadyMade) {
 }
 
 btn.addEventListener("click", () => {
-  const page = `<a class="href" href="${link.value}"><img class="image" src="${imageLink.value}"></a>`;
+  const page = `<a class="href" href="${link.value}"><img class="image" src="${imageLink.value}"></a> <div class="edit" id="edit"></div>`;
   document.body.innerHTML = page;
   localStorage.setItem("image", page);
+
+  const edit = document.getElementById('edit')
+  edit.addEventListener('click', ()=> {
+      localStorage.setItem('image', '')
+      location.reload()
+  })
 });
+// localStorage.setItem('image', "")
