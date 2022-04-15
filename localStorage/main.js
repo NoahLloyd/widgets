c45cb96837315c21b066e633676a4026e464a464
Noah Lloyd <noahlloydrobson@gmail.com>
2022-04-15 20:22:03 +0200

    testing localStorage in notion embed

---
+++ localStorage/main.js
@@ -0,0 +1,12 @@
+const number = document.getElementById("num");
+const display = document.getElementById("display");
+
+const count = localStorage.getItem("count");
+count ? (display.innerHTML = count) : (display.innerHTML = 0);
+
+number.addEventListener("change", (e) => {
+  display.innerHTML = e.target.value;
+  localStorage.setItem("count", e.target.value);
+});
+
+// Want to make a counter
