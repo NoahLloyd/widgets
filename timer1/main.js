e20a0c1bfa9f1e51c7d68ad66c9748a36675102e
Noah Lloyd <noahlloydrobson@gmail.com>
2022-07-31 22:30:00 +0200

    1 minute timer

---
+++ timer1/main.js
@@ -0,0 +1,74 @@
+const TIME_IN_SECONDS = 60
+let time = TIME_IN_SECONDS
+let setTime = TIME_IN_SECONDS
+let running = false
+let runningInterval = null
+const timeElement = document.getElementById('time')
+const restartButton = document.getElementById('restart')
+const startButton = document.getElementById('startSession')
+
+startButton.addEventListener('click', () => {toggleSession()})
+
+const toggleSession = () => {
+    if(running) {
+        pauseSession()
+    } else {
+        startSession()
+    }
+}
+
+const startSession = () => {
+    running = true
+    startButton.innerText = "Pause"
+
+    let startTime = new Date()
+    let now = new Date()
+    const interval = setInterval(() => {
+        let now2 = new Date()
+
+        if(now2.getSeconds() > now.getSeconds()) {
+            time = setTime + Math.floor(startTime.getTime() / 1000) - Math.floor(now2.getTime() / 1000)
+            updateTime(time)
+        }
+
+        if (time === 0) {
+            new Audio('./ringing.wav').play()
+            restartSession()
+        }
+
+        now = now2
+    }, 1000) 
+    runningInterval = interval
+}
+
+const pauseSession = () => {
+    clearInterval(runningInterval)
+    startButton.innerText = "Start"
+    running = false
+    setTime = time
+}
+const restartSession = () => {
+    if (running) pauseSession()
+    time = TIME_IN_SECONDS
+    setTime = TIME_IN_SECONDS
+    updateTime()
+}
+
+restartButton.addEventListener('click', () => {
+    restartSession()
+})
+
+
+
+
+const updateTime = () => {
+    const minutes = Math.floor(time/60)
+    const seconds = time % 60
+    const secondsLength = `${seconds}`.length
+
+    // Adds 0's in front in case the number doesn't fill 2 characters
+    const secondsDisplayed =  secondsLength === 0 ? "00" : secondsLength === 1 ? `0${seconds}` : seconds
+    timeElement.innerText = `${minutes}:${secondsDisplayed}`
+}
+
+updateTime()
